be85ec313072b88f870155d78aa9ab36dd0a3a56
SACHIN <sachin@SACHINs-MacBook-Pro.local>
2025-10-03 19:45:13 +0530

    Base Frontend Setup

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,34 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { resume } from "react-dom/server";
+import { resumes } from "~/contants";
+import ResumeCard from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "AI-Resume" },
+    { name: "description", content: "AI Powered Resume Tracking System" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar/>
+
+    <section className="main-section">
+      <div className="page-heading py-16">
+        <h1>Track Your Applications and Resume Ratings</h1>
+        <h2>Review Your Submission and check AI-Powered Feedback.</h2>
+      </div>
+    
+
+    {resume.length > 0 && (
+    <div className="resumes-section">
+      {resumes.map((resume) => (
+        <ResumeCard key={resume.id} resume={resume} />
+      ))}
+    </div>
+    )}
+  </section>
+  </main>;
 }
